de901af003404eee39cdb20d4cf49bf313c63038
meve19 <k5_19_3b@hotmail.com>
2025-04-28 01:44:11 +0900

    実験9

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,7 @@ export default defineConfig({
   root: './',  // プロジェクトのルートを設定
   base: './',  // 相対パスに設定
   publicDir: 'public',  // publicディレクトリを明示的に指定
+  build: {
+    outDir: 'dist', // 出力先ディレクトリをdistに設定
+  },
 });
