2c5c9b084514d819283723294f1c478b711e58f5
meve19 <k5_19_3b@hotmail.com>
2025-04-28 01:17:09 +0900

    実験4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  base: './', // baseを相対パスに設定
+  root: 'src', // srcディレクトリをルートディレクトリに設定
+});
