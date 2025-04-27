cc2c18ff5a1bae25cd502d4a38414bb80b4c6d69
meve19 <k5_19_3b@hotmail.com>
2025-04-28 01:23:51 +0900

    実験7

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './',  // これを追加して、プロジェクトのルートを設定
-  base: './',  // baseを相対パスに設定
+  root: './',  // プロジェクトのルートを設定
+  base: './',  // 相対パスに設定
+  publicDir: 'public',  // publicディレクトリを明示的に指定
 });
